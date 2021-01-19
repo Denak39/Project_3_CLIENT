4c9d9d5130660a2b9f2100ae21ddf4c808266649
Mathias Valverde <mathiasvalverde@hotmail.fr>
2021-01-19 18:35:10 +0100

    added list of lessons title in profilementor

--- src/pages/ProfileMentor.jsx
+++ src/pages/ProfileMentor.jsx
@@ -1,37 +1,35 @@
 import React from "react";
 import { withUser } from "../components/Auth/withUser";
 
-
 const ProfileMentor = (props) => {
-console.log("IM in mentor");
-
+  console.log("IM in mentor");
 
   return (
-    
-      <div>
-      <h1>Ded-Sec</h1> 
-      <hr/>
+    <div>
+      <h1>Ded-Sec</h1>
+      <hr />
       <h1>Mentor Space</h1>
-      <img src="../../public/teacher.png" alt="mentor"/>
+      <img src="../../public/teacher.png" alt="mentor" />
       <p>Mentor privileges:</p>
       <ul>
         <li>Create exchange rooms, learning material and other supports.</li>
-        <li>Participate to the community as an influent speaker, be an ambassador of DedSec Webwide.</li>
+        <li>
+          Contribute to the community as an influent speaker, be an ambassador
+          of DedSec Webwide.
+        </li>
       </ul>
-      <hr/>
+      <hr />
       <div>
         <h3>Userinfo:</h3>
         <p>Username: {props.context.user.username}</p>
-        <p>Account Type: {props.context.user.accountType}</p>
+        {/* <p>Account Type: {props.context.user.accountType}</p> */}
         <p>Avatar: {props.context.user.profileImg}</p>
         <p>Email: {props.context.user.email}</p>
       </div>
       <button>Create Lesson</button>
+      <h3>List of lessons you posted:</h3>
     </div>
   );
 };
-
-
-
 
 export default withUser(ProfileMentor);
